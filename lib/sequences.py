3296326ce35fe573cd074c37f18ca5465ecf013a
sidee <said.issack@student.moringaschool.com>
2023-08-20 23:48:47 +0300

    completed assighnment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,11 @@
 
 def print_fibonacci(length):
     pass
+    fib_sequence = [0, 1]  
+    while len(fib_sequence) < length:
+        next_number = fib_sequence[-1] + fib_sequence[-2]  
+        fib_sequence.append(next_number) 
+    print(fib_sequence)
+
+
+print_fibonacci(10)  
